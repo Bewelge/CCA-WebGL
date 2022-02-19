3a35c2941001147a301616b7b7cd1e0d4dac288c
bewelge <17884995+Bewelge@users.noreply.github.com>
2022-02-19 19:04:56 +0100

    twoway heatsink

--- src/js/StartingPattern.js
+++ src/js/StartingPattern.js
@@ -136,17 +136,34 @@ class StartingPattern {
 	}
 	heatSink() {
 		this.setStyle()
-		let p = new Vec2(rndInt(0, this.w), rndInt(0, this.h))
-		let w = 15 * rndInt(15, 50)
-		let c = this.c
-		c.strokeStyle =
-			"rgba(" + rndInt(0, 5) + "," + rndInt(0, 5) + "," + rndInt(0, 5) + ",1)"
-		for (let i = 0; i < w; i += 15) {
-			c.beginPath()
-			c.moveTo(p.x - w / 2 + i, p.y - w / 2)
-			c.lineTo(p.x - w / 2 + i, p.y + w / 2)
-			c.stroke()
-			c.closePath()
+
+		if (rndFloat(0, 1) < 0.5) {
+			let p = new Vec2(rndInt(0, this.w), rndInt(0, this.h))
+			let w = 15 * rndInt(15, 50)
+			let c = this.c
+			c.strokeStyle =
+				"rgba(" + rndInt(0, 5) + "," + rndInt(0, 5) + "," + rndInt(0, 5) + ",1)"
+			for (let i = 0; i < w; i += 15) {
+				c.beginPath()
+				c.moveTo(p.x - w / 2 + i, p.y - w / 2)
+				c.lineTo(p.x - w / 2 + i, p.y + w / 2)
+				c.stroke()
+				c.closePath()
+			}
+		}
+		if (rndFloat(0, 1) < 0.5) {
+			let p = new Vec2(rndInt(0, this.w), rndInt(0, this.h))
+			let w = 15 * rndInt(15, 50)
+			let c = this.c
+			c.strokeStyle =
+				"rgba(" + rndInt(0, 5) + "," + rndInt(0, 5) + "," + rndInt(0, 5) + ",1)"
+			for (let i = 0; i < w; i += 15) {
+				c.beginPath()
+				c.moveTo(p.x - w / 2, p.y - w / 2 + i)
+				c.lineTo(p.x + w / 2, p.y - w / 2 + i)
+				c.stroke()
+				c.closePath()
+			}
 		}
 	}
 	randomLines() {
